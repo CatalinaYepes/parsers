ac082458c61e3b68d6656429b813ec05d5ea6e70
CatalinaYepes <catalina.yepes@globalquakemodel.org>
2017-04-18 12:21:22 +0200

    Import modeule from python path

--- parsers_exposure/test/test_parse_variables.py
+++ parsers_exposure/test/test_parse_variables.py
@@ -4,14 +4,15 @@ Testing code
 """
 import pandas as pd
 
-from mapping import mapping_matrix
-from variable_1 import parse_1var
-from variable_3 import census_3var_Crossed
-import variable_2
+from parsers_exposure.mapping import mapping_matrix
+from parsers_exposure.variable_1 import parse_1var
+from parsers_exposure.variable_3 import census_3var_Crossed
+from parsers_exposure import variable_2
+from parsers_exposure.get_values import reshape_expo_data
 
 
 #%% TEST 1 VARIABLE
-data_file = 'ex_data-noncrossed.xlsx'
+data_file = '../ex_data-noncrossed.xlsx'
 data = pd.read_excel(data_file, sheetname='variable_1')
 mapping = mapping_matrix(data_file, num_variables='one', sheetname='mapping_1v', header=1)
 result_1var = parse_1var(data, mapping)
@@ -20,7 +21,7 @@ print result_1var.head(5)
 
 
 #%% TEST 2 VARIABLES - NON-CROSSED VARS
-data_file = 'ex_data-noncrossed.xlsx'
+data_file = '../ex_data-noncrossed.xlsx'
 data_var1 = pd.read_excel(data_file, sheetname=0)
 data_var2 = pd.read_excel(data_file, sheetname=1)
 mapping = mapping_matrix(data_file, num_variables='two', sheetname='mapping_1v_2v', header=1)
@@ -31,8 +32,8 @@ print result_2var.head(5)
 
 
 #%% TEST 2 VARIABLES - CROSSED VARS
-data_file = 'ex_data-crossed.xlsx'
-data = pd.read_excel(data_file, sheetname='Level1_2var')
+data_file = '../ex_data-crossed.xlsx'
+data = pd.read_excel(data_file, sheetname='data_2vars')
 mapping = mapping_matrix(data_file, num_variables='two', sheetname='mapping_2var', header=2)
 
 result_2var = variable_2.census_2var_Crossed(data, mapping)
@@ -41,7 +42,7 @@ print result_2var.head(5)
 
 
 #%% TEST 3 VARIABLES - CROSSED VARS
-data_file = 'ex_data-crossed.xlsx'
+data_file = '../ex_data-crossed.xlsx'
 data = pd.read_excel(data_file, sheetname='data_3vars')
 mapping = mapping_matrix(data_file, num_variables='three', row_var3=9, sheetname='mapping_3var', header=2)
 
