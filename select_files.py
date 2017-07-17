3bda07a690df8c429b6c1fa75a8ae7f6ba157c64
CatalinaYepes <catalina.yepes@globalquakemodel.org>
2017-07-17 16:37:02 +0200

    include documentation

--- select_files.py
+++ select_files.py
@@ -8,6 +8,18 @@ Select files inside a folder
 import os
 
 def select_files(folder=".", start="", end="", contain=""):
+    '''Function to select files inside a folder
+        
+        :folder: folder name, by default current one
+
+        :start: select the files that start with a given string
+
+        :end: select the files that end with a given string
+
+        :contain: select the files that contain a given string
+
+        Returns a list_names of files if more than one
+    '''
     files = []
     for file_name in os.listdir(folder):
         if file_name.startswith(start):
